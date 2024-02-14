3c6c5cea739eeaf10479b5dfd7f6b689ea083c0c
Yugi00 <jyep09@gmail.com>
2024-02-13 17:07:57 -0800

    putting files onto github

---
+++ flask-on-docker-dir/services/web/manage.py
@@ -0,0 +1,26 @@
+from flask.cli import FlaskGroup
+
+from project import app, db, User
+
+
+cli = FlaskGroup(app)
+
+
+@cli.command("create_db")
+def create_db():
+    db.drop_all()
+    print("dropped all")
+    db.create_all()
+    print("created all")
+    db.session.commit()
+    print("committed all")
+
+
+@cli.command("seed_db")
+def seed_db():
+    db.session.add(User(email="michael@mherman.org"))
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    cli()
